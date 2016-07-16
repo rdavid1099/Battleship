1ec2f9cec8d5c74ae21d8ae38286c1f6197adfd6
Ryan Workman <rdavid1099@gmail.com>
2016-07-16 09:17:03 -0600

    Added functionality to game board.  Still need to implement output functionality.

--- lib/game_board.rb
+++ lib/game_board.rb
@@ -1,13 +1,17 @@
+require 'pry'
+
 class GameBoard
-  attr_reader :blank_game_board
+  attr_reader :current_game_board
 
   def initialize(board_size = 4)
     @board_size = board_size
-    @blank_game_board = Array.new
+    create_new_board
   end
 
-  def create_new_board
-    @board_size.times { @blank_game_board << generate_line }
+  def create_new_board(new_size = @board_size)
+    @board_size = new_size
+    @current_game_board = []
+    @board_size.times { @current_game_board << generate_line }
   end
 
   def generate_line
@@ -17,7 +21,24 @@ class GameBoard
   end
 
   def size
-    blank_game_board.length
+    current_game_board.length
+  end
+
+  def mark_shot(x_coordinate, y_cooridnate)
+    if valid_shot?(x_coordinate, y_cooridnate)
+      @current_game_board[x_coordinate][y_cooridnate] = 'S'
+      return [x_coordinate, y_cooridnate]
+    else
+      return "Coordinates already shot"
+    end
+  end
+
+  def valid_shot?(x_coordinate, y_cooridnate)
+    return true unless @current_game_board[x_coordinate][y_cooridnate] == 'S'
+  end
+
+  def clear_game_board
+    create_new_board
   end
 
 end
--- test/game_board_test.rb
+++ test/game_board_test.rb
@@ -32,7 +32,94 @@ class TestGameBoard < Minitest::Test
     assert_equal [[".",".",".","."],
                   [".",".",".","."],
                   [".",".",".","."],
-                  [".",".",".","."]], @g.blank_game_board
+                  [".",".",".","."]], @g.current_game_board
   end
 
+  def test_game_board_automatically_generated_when_initialized
+    gameboard = GameBoard.new
+    assert_equal [[".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."]], @g.current_game_board
+  end
+
+  def test_game_board_size_changes_depending_on_initialize_size
+    gameboard = GameBoard.new(3)
+    assert_equal [[".",".","."],
+                  [".",".","."],
+                  [".",".","."]], gameboard.current_game_board
+  end
+
+  def test_game_board_can_also_be_manually_generated
+    gameboard = GameBoard.new
+    assert_equal [[".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."]], gameboard.current_game_board
+    gameboard.create_new_board(3)
+    assert_equal [[".",".","."],
+                  [".",".","."],
+                  [".",".","."]], gameboard.current_game_board
+    gameboard.create_new_board(4)
+    assert_equal [[".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."]], gameboard.current_game_board
+  end
+
+  def test_board_marks_shots_fired
+    @g.mark_shot(0,0)
+    assert_equal [["S",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."]], @g.current_game_board
+  end
+
+  def test_board_marks_two_shots
+    @g.mark_shot(0,0)
+    @g.mark_shot(1,2)
+    assert_equal [["S",".",".","."],
+                  [".",".","S","."],
+                  [".",".",".","."],
+                  [".",".",".","."]], @g.current_game_board
+  end
+
+  def test_board_marks_five_shots
+    @g.mark_shot(0,2)
+    @g.mark_shot(1,3)
+    @g.mark_shot(3,2)
+    @g.mark_shot(0,1)
+    @g.mark_shot(1,2)
+    assert_equal [[".","S","S","."],
+                  [".",".","S","S"],
+                  [".",".",".","."],
+                  [".",".","S","."]], @g.current_game_board
+  end
+
+  def test_board_determines_if_shot_is_valid
+    @g.mark_shot(0,0)
+    refute @g.valid_shot?(0,0)
+    assert @g.valid_shot?(1,1)
+  end
+
+  def test_board_allows_shot_unless_its_not_valid
+    @g.mark_shot(0,0)
+    refute @g.valid_shot?(0,0)
+    assert_equal "Coordinates already shot", @g.mark_shot(0,0)
+  end
+
+  def test_board_returns_coordinates_if_shot_is_valid_and_marked
+    assert_equal [0,0], @g.mark_shot(0,0)
+    assert_equal [1,2], @g.mark_shot(1,2)
+    assert_equal [3,3], @g.mark_shot(3,3)
+    assert_equal "Coordinates already shot", @g.mark_shot(1,2)
+  end
+
+  def test_game_board_can_be_cleared
+    @g.clear_game_board
+    assert_equal [[".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."],
+                  [".",".",".","."]], @g.current_game_board
+  end
 end
